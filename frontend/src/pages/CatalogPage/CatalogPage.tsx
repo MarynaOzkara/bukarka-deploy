657723935ecce7389d86499a5ec11d0d8123c85a
tanyushanel <tatyana.nelipovich@gmail.com>
2024-06-10 21:09:25 +0200

    feat: restructuring catalogpage with outlet

--- frontend/src/pages/CatalogPage/CatalogPage.tsx
+++ frontend/src/pages/CatalogPage/CatalogPage.tsx
@@ -1,67 +1,12 @@
-import { useBooks } from "components/Book";
-import React, { useEffect } from "react";
-import { useParams, useSearchParams } from "react-router-dom";
-import { BookCard, Pagination } from "components";
-import {
-  FlexWrapper,
-  PageWrapper,
-  StyledCommonWrapper,
-  Wrapper,
-} from "styles/CommonStyled";
-import { BreadCrumbs, Label } from "../CommonPages.styled";
+import React from "react";
+import { Outlet } from "react-router-dom";
+import { PageWrapper, StyledCommonWrapper } from "styles/CommonStyled";
 
 const CatalogPage: React.FC = () => {
-  const { books, fetchBooks, currentPage, setCurrentPage, totalPages } =
-    useBooks();
-  const [searchParams, setSearchParams] = useSearchParams();
-
-  const { category, subcategory, link } = useParams();
-
-  console.log(category);
-
-  useEffect(() => {
-    const page = searchParams.get("page")
-      ? Number(searchParams.get("page"))
-      : 1;
-    if (page !== currentPage) {
-      setCurrentPage(page);
-    } else {
-      fetchBooks(page);
-    }
-  }, [searchParams, currentPage, setCurrentPage, fetchBooks]);
-
-  const handlePageChange = (page: number) => {
-    setCurrentPage(page);
-    setSearchParams({ page: page.toString() });
-  };
-
   return (
     <StyledCommonWrapper>
       <PageWrapper>
-        <Wrapper>
-          <BreadCrumbs>Каталог | {category} </BreadCrumbs>
-          <Label> {link || subcategory || category || "Усі книги"} </Label>
-
-          <FlexWrapper
-            style={{
-              justifyContent: "space-around",
-              flexWrap: "wrap",
-              gap: "2rem",
-            }}
-          >
-            {(books.length > 0 &&
-              books.map((book, index) => (
-                <BookCard key={index} {...book} />
-              ))) || <div>No books in catalog</div>}
-          </FlexWrapper>
-          {books.length > 0 && (
-            <Pagination
-              currentPage={currentPage}
-              totalPages={totalPages}
-              onPageChange={handlePageChange}
-            />
-          )}
-        </Wrapper>
+        <Outlet />
       </PageWrapper>
     </StyledCommonWrapper>
   );
--- frontend/src/pages/CatalogPage/SectionPage/SectionPage.tsx
+++ frontend/src/pages/CatalogPage/SectionPage/SectionPage.tsx
@@ -1,14 +1,56 @@
-import { useParams } from "react-router-dom";
+import { BookCard, Pagination } from "components";
+import { useBooks } from "components/Book";
+import { BreadCrumbs, Label } from "pages/CommonPages.styled";
+import { useEffect } from "react";
+import { useParams, useSearchParams } from "react-router-dom";
+import { FlexWrapper, Wrapper } from "styles/CommonStyled";
 
 const SectionPage = () => {
+  const { books, fetchBooks, currentPage, setCurrentPage, totalPages } =
+    useBooks();
   const { category, subcategory, link } = useParams();
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  useEffect(() => {
+    const page = searchParams.get("page")
+      ? Number(searchParams.get("page"))
+      : 1;
+    if (page !== currentPage) {
+      setCurrentPage(page);
+    } else {
+      fetchBooks(page);
+    }
+  }, [searchParams, currentPage, setCurrentPage, fetchBooks]);
+
+  const handlePageChange = (page: number) => {
+    setCurrentPage(page);
+    setSearchParams({ page: page.toString() });
+  };
 
   return (
-    <div>
-      <h2>{category}</h2>
-      {subcategory && <h3>{subcategory}</h3>}
-      {link && <h4>{link}</h4>}
-    </div>
+    <Wrapper>
+      <BreadCrumbs>Каталог | {category} </BreadCrumbs>
+      <Label> {link || subcategory || category || "Усі книги"} </Label>
+      <FlexWrapper
+        style={{
+          justifyContent: "space-around",
+          flexWrap: "wrap",
+          gap: "2rem",
+        }}
+      >
+        {(books.length > 0 &&
+          books.map((book, index) => <BookCard key={index} {...book} />)) || (
+          <div>No books in catalog</div>
+        )}
+      </FlexWrapper>
+      {books.length > 0 && (
+        <Pagination
+          currentPage={currentPage}
+          totalPages={totalPages}
+          onPageChange={handlePageChange}
+        />
+      )}
+    </Wrapper>
   );
 };
 
