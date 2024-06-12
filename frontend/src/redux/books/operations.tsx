00f09ab8c5c13ba99a35fe52e2641d92f976a7ca
Olena Pobehailo <106855523+OlenaPobehailo@users.noreply.github.com>
2024-06-12 11:13:04 +0300

    fix PaymentPage and OrderConfirmationPage

--- frontend/src/redux/books/operations.tsx
+++ frontend/src/redux/books/operations.tsx
@@ -1,12 +1,10 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
-import axios from "axios";
+import { instance } from "utils/fetchInstance";
 
 export const fetchBooksList = createAsyncThunk(
   "books/fetchBooksList",
   async (_, thunkAPI) => {
-    const response = await axios.get(
-      "https://bukarka.onrender.com/api/books/new",
-    );
+    const response = await instance.get(`/api/books/new`);
     return response.data.data;
-  },
+  }
 );
