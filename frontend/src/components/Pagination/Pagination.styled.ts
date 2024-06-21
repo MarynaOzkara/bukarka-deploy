061dd826fe8db5158cdbacebfec6d9b4bfbce8c0
tanyushanel <tatyana.nelipovich@gmail.com>
2024-06-21 12:03:45 +0200

    feat: restyling pagination

--- frontend/src/components/Pagination/Pagination.styled.ts
+++ frontend/src/components/Pagination/Pagination.styled.ts
@@ -2,8 +2,17 @@ import styled from "styled-components";
 import { FlexWrapper } from "styles/CommonStyled";
 
 export const PaginationWrapper = styled(FlexWrapper)`
+  gap: 2rem;
   margin: 2rem auto;
   max-width: 15rem;
   color: var(--bukarka-dark-grey);
   align-items: center;
+
+  button {
+    padding: 0.5rem;
+  }
+
+  .active {
+    background: var(--bukarka-yellow);
+  }
 `;
