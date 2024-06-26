834ebb9f83ae0713c9d334ea8d400982a206ce80
tanyushanel <tatyana.nelipovich@gmail.com>
2024-06-26 18:08:02 +0200

    fix styles

--- frontend/src/pages/BookPage/PictureViewer/PictureViewer.styled.ts
+++ frontend/src/pages/BookPage/PictureViewer/PictureViewer.styled.ts
@@ -3,9 +3,12 @@ import { FlexWrapper } from "styles/CommonStyled";
 import { BookImage } from "../BookPage.styled";
 
 export const StyledFlexWrapper = styled(FlexWrapper)`
-  padding: 50px;
+  padding: 50px 0;
+  min-width: 616px;
+  justify-content: center;
   gap: 3rem;
   align-items: center;
+
   & span {
     font-size: 3rem;
     font-weight: 300;
@@ -21,6 +24,5 @@ export const StyledBookImage = styled(BookImage)`
   & img {
     height: 672px;
     width: 440px;
-    object-fit: cover;
   }
 `;
