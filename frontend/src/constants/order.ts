a853da08701ddc975d6e146d0e4cc54afb51cf75
Olena Pobehailo <106855523+OlenaPobehailo@users.noreply.github.com>
2024-06-29 19:31:04 +0300

    delete ukrposhta

--- frontend/src/constants/order.ts
+++ frontend/src/constants/order.ts
@@ -5,7 +5,6 @@ export const PAYMENT_METHOD = {
 };
 
 export const DELIVERY_METHOD = {
-  ukrPoshta: "Самовивіз з відділення Укрпошти",
   novaPoshtaWarehouse: "Самовивіз з відділення Нової Пошти",
   novaPoshtaParcelLocker: "Самовивіз з поштомату Нової Пошти",
   novaPoshtaCourier: "Доставка кур’єром Нової Пошти",
