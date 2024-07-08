031a2f0bba73af749ac2376dd2755f94bc1e69cb
Olena Pobehailo <106855523+OlenaPobehailo@users.noreply.github.com>
2024-07-09 00:07:02 +0300

    update Search + delete stroke from lens-icon.svg

--- frontend/src/components/Search/Search.styled.ts
+++ frontend/src/components/Search/Search.styled.ts
@@ -11,8 +11,9 @@ export const StyledForm = styled.form`
 `;
 
 export const SearchInput = styled(Input)`
-  padding: 0 16px 0 48px;
-  width: 400px;
+  height: 40px;
+  width: 200px;
+  padding: 0 8px;
 
   border: none;
 
@@ -22,26 +23,57 @@ export const SearchInput = styled(Input)`
     line-height: 150%;
     color: var(--bukarka-dark-grey);
   }
+
+  @media (min-width: 768px) {
+    width: 400px;
+    padding: 0 16px 0 48px;
+  }
 `;
 
 export const StyledLensIcon = styled(LensIcon)`
-  position: absolute;
-  top: 50%;
-  transform: translateY(-50%);
-  left: 16px;
+  display: none;
+
+  @media (min-width: 768px) {
+    display: inline-block;
+
+    position: absolute;
+    top: 50%;
+    transform: translateY(-50%);
+    left: 16px;
+
+    stroke: var(--bukarka-grey);
+  }
 `;
 
 export const FormButton = styled.button`
-  padding: 8px 21px;
   border: none;
-
   background: var(--bukarka-orange);
   color: var(--bukarka-black);
+`;
 
-  font-family: var(--semibold);
-  font-size: 16px;
-  line-height: 150%;
-  color: var(--bukarka-black);
+export const FormButtonMobile = styled(FormButton)`
+  padding: 2px 4px;
+
+  & svg {
+    stroke: var(--bukarka-black);
+  }
+
+  @media (min-width: 768px) {
+    display: none;
+  }
+`;
+
+export const FormButtonDesktop = styled(FormButton)`
+  display: none;
+
+  @media (min-width: 768px) {
+    display: inline-block;
+    padding: 8px 21px;
+
+    font-family: var(--semibold);
+    font-size: 16px;
+    line-height: 150%;
+  }
 `;
 
 export const StyledHints = styled(Hints)`
